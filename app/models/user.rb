fbbc29b53b6cfafec9ea9a6138cc08b4b4f9b4a9
Joel Feinstein <jnfeinstein@gmail.com>
2013-09-10 22:36:00 -0700

    Downcase all emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,15 +20,20 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, uniqueness: true
   validates :password_hash, presence: true
   before_create :build_default_associations
+  before_validation :downcase_email
 
   def self.authenticate(user_email, user_password)
-    user = User.find_by_email(user_email)
+    user = User.find_by_email(user_email.downcase)
     user if user && user.password == user_password
   end
 
   def build_default_associations
     self.build_bank
     self.build_bujit
+  end
+
+  def downcase_email
+    self.email = self.email.downcase if self.email.present?
   end
 
 	def password
