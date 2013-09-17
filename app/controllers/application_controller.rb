49a96c9018fed092eca1e6b13b85335496d1b78c
joelf <joelf@meraki.net>
2013-09-17 11:45:07 -0700

    Disable csrf protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,10 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
 
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  respond_to :xml, :json
+  before_filter :check_token
 
   helper_method :current_user
-  respond_to :xml, :json
-
-  before_filter :check_token
 
   def check_token
     user = User.find_by_email(cookies[:user])
